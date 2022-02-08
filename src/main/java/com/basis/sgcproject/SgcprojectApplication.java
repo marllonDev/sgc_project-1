ac984d9aa55506497655f5c97fa42293dbbd3791
/Jeronimo <jeronimo_espala@hotmail.com>
2022-02-07 21:37:00 -0300

    Config project

---
+++ src/main/java/com/basis/sgcproject/SgcprojectApplication.java
@@ -0,0 +1,13 @@
+package com.basis.sgcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcprojectApplication.class, args);
+	}
+
+}
