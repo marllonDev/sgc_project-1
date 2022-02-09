c212f5665f03b251960c71ad79e8425a3c73a435
Mgfs07 <matheusantos07@hotmail.com>
2022-02-09 15:47:49 -0300

    Concerto de codigo, fiz os inserts tabela categoria, apaguei algumas class, adicionei competencias no pom

--- src/main/java/com/basis/sgcproject/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.basis.sgcproject.repository;
-
-import com.basis.sgcproject.domain.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-}
--- src/main/java/com/basis/sgcproject/service/dto/CategoriaDTO.java
+++
@@ -1,8 +0,0 @@
-package com.basis.sgcproject.service.dto;
-
-public class CategoriaDTO {
-
-    private Integer id;
-
-    private String nome;
-}
