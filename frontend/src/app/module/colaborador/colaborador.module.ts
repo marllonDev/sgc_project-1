fa153d7dd28606b26651b16989f0940ab071af46
/Jeronimo <jeronimo_espala@hotmail.com>
2022-02-14 23:06:15 -0300

    Create Frontend

---
+++ frontend/src/app/module/colaborador/colaborador.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ColaboradorRoutingModule } from './colaborador-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ColaboradorRoutingModule
+  ]
+})
+export class ColaboradorModule { }
