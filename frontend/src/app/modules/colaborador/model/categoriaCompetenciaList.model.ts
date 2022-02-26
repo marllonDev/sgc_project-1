abe4141a818e7f8363929e073214198905d74822
Marllon Zucolotto <marlonzucolotto@gmail.com>
2022-02-26 19:16:45 -0300

    Foto finalizado.

--- frontend/src/app/modules/colaborador/model/categoriaCompetenciaList.model.ts
+++ frontend/src/app/modules/colaborador/model/categoriaCompetenciaList.model.ts
@@ -1,6 +1,6 @@
 import { CompetenciaModel } from 'src/app/modules/competencia/model/competencia.models';
-import { CategoriaModel } from './../../competencia/model/categoria.model';
 
 export class CategoriaCompetenciaListModel{
+    nivel: string;
     competencia: CompetenciaModel;
 }
