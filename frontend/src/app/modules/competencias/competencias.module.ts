b5b9d00ff2b09af09a8c06a865736cc9246dcffc
/Jeronimo <jeronimo_espala@hotmail.com>
2022-02-27 11:48:01 -0300

    Frontend Competencia

--- frontend/src/app/modules/competencias/competencias.module.ts
+++ frontend/src/app/modules/competencias/competencias.module.ts
@@ -6,18 +6,16 @@ import { CompetenciasRoutingModule } from './competencias-routing.module';
 import { CompetenciasComponent } from './competencias-list/competencias.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CompetenciasFormComponent } from './competencias-form/competencias-form.component';
+import { ConfirmationService } from 'primeng';
+
 
 @NgModule({
-  declarations: [
-    CompetenciasComponent,
-    CompetenciasFormComponent
-  ],
+  declarations: [CompetenciasComponent, CompetenciasFormComponent],
   imports: [
     CommonModule,
     CompetenciasRoutingModule,
     SharedModule
-  
-  
-  ]
+  ],
+  providers: [ConfirmationService]
 })
 export class CompetenciasModule { }
